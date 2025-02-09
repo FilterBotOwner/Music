ac1cd61bb212e34b763c447f0a5aaf74a7866408
FilterBotOwner <zeba2862@gmail.com>
2025-02-10 04:50:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-  return "Bot is Working..."
+def hello_world():
+    return 'Hello from Tech VJ'
+
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0")
+    app.run()
+  
