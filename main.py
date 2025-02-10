4ddeeed3a745377f3d5b8d26ee23522e5cb9f1b9
FilterBotOwner <zeba2862@gmail.com>
2025-02-10 05:46:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-from bot import Bot
+import asyncio
+from bot import Bot  # Ensure you have a correct import
 
-Bot().run() 
+bot = Bot()
+
+async def main():
+    await bot.run()  # Ensure 'run()' is an async function
+
+asyncio.run(main())
